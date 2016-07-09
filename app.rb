917f548d412e97a66d29173504aa0aa4f63740ca
Sergio Vilar <sergiovilar.r@gmail.com>
2016-07-09 10:41:12 -0300

    Added support to environment variables

--- app.rb
+++ app.rb
@@ -3,12 +3,28 @@ require 'yaml'
 require 'json'
 require 'time'
 
-config = YAML.load_file('config.yml')
+def get_channel(channel, config)
+  if ENV.has_key?("#{channel.upcase}_VERSION")
+    {
+      'version' => ENV["#{channel.upcase}_VERSION"],
+      'notes' => ENV["#{channel.upcase}_NOTES"],
+      'pub_date' => ENV["#{channel.upcase}_DATE"]
+    }
+  elsif config['updates'][channel] && !config['updates'][channel].empty?
+    config['updates'][channel]
+  else
+    false
+  end
+end
+
+config = YAML.load_file('config.yml') unless ENV.has_key?('STABLE_VERSION')
+base_path = ENV.has_key?('BASE_PATH') ? ENV['BASE_PATH'] : config['environment']['base_path']
 
 get '/update/:channel/:version' do |channel, version|
-  parameters_empty = channel.empty? || version.empty? || !config['updates'][channel] || config['updates'][channel].empty?
+  channel_data = get_channel(channel, config)
+  parameters_empty = channel.empty? || version.empty? || !channel_data
   begin
-    no_need_for_update = Gem::Version.new(version) >= Gem::Version.new(config['updates'][channel]['version'])
+    no_need_for_update = Gem::Version.new(version) >= Gem::Version.new(channel_data['version'])
   rescue
     no_need_for_update = true
   end
@@ -16,13 +32,12 @@ get '/update/:channel/:version' do |channel, version|
     status 204
     ''
   else
-    release = config['updates'][channel]
     content_type :json
     {
-      url: "#{config['environment']['base_path']}/#{channel}/#{release['version']}.zip",
-      name: release['version'],
-      notes: release['notes'],
-      pub_date: Time.parse(release['pub_date']).iso8601
+      url: "#{base_path}/#{channel}/#{channel_data['version']}.zip",
+      name: channel_data['version'],
+      notes: channel_data['notes'],
+      pub_date: Time.parse(channel_data['pub_date']).iso8601
     }.to_json
   end
 end
